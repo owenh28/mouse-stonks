eddebeec50f9edc04d2f6b0bc7df3335be8238cb
owenh <owenharlacker@outlook.com>
2022-03-31 23:38:43 -0600

    cv2 stuff

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print('test')
+import cv2
+import numpy as np
+
+def main():
+    video = cv2.VideoCapture(4)
+    cv2.namedWindow('image')
+    while 1:
+        ret, cam = video.read()
+        #videoThresh = cv2.threshold(cam, 150,255, cv2.THRESH_BINARY)
+        cv2.imshow('image', cam)
+
+
+
+
+
+if __name__ == '__main__':
+    main()
