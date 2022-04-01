8cbf07e94188d136c18d04c8598b0daf6cf6350d
owenh <owenharlacker@outlook.com>
2022-04-01 12:35:51 -0600

    yayayayay i got imaging working

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import cv2
+import time
 #import numpy as np
 
 def main():
-    video = cv2.VideoCapture(4)
-    cv2.namedWindow('image')
+    video = cv2.VideoCapture(0)
     while 1:
         ret, cam = video.read()
-        #videoThresh = cv2.threshold(cam, 150,255, cv2.THRESH_BINARY)
-        cv2.imshow('image', cam)
+        if ret:
+            bw = cv2.cvtColor(cam, cv2.COLOR_BGR2GRAY)
+            videoThresh = cv2.threshold(bw, 140, 255, cv2.THRESH_BINARY)[1]
+            time.sleep(1 / 30)
+
+            cv2.imshow('frame', videoThresh)
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
 
 
 
